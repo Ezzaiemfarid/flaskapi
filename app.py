45b07ccf4eb94e26fa2a4e051a9529d72245dd7f
DAHMENatef <66395189+DAHMENatef@users.noreply.github.com>
2020-07-05 17:46:37 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from tensorflow import keras
+import numpy as np
+model = keras.models.load_model('m.h5')
+def predict(IS, pachymetrie, pattern, couleur, DBFS):
+    yy = np.array([[IS, pachymetrie, pattern, couleur, DBFS]], np.float)
+
+    prediction = model.predict(yy, batch_size=None, verbose=0, steps=None)
+    print(prediction)
+    return prediction
+app = Flask(__name__)
+
+
+@app.route('/j',methods=['POST','GET'])
+def hello_world():
+    req_data = request.get_json()
+    a = req_data['a']
+    b = req_data['b']
+    c = req_data['c']
+    d = req_data['d']
+    e = req_data['e']
+    x = predict(float(a),float(b),float(c),float(d),float(e))[0][0]
+    return jsonify(str(x))
+
+
+if __name__ == '__main__':
+    app.run()
