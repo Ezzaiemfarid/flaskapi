44a00bc03f6df8b18017d26f046ef2bf6f6c70a8
DAHMENatef <atefdahmen0@gmail.com>
2020-12-17 02:27:45 +0100

    okii

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from tensorflow import keras
+f"""rom tensorflow import keras
 import numpy as np
 
 model = keras.models.load_model('m.h5')
@@ -10,14 +10,14 @@ def predict(IS, pachymetrie, pattern, couleur, DBFS):
 
     prediction = model.predict(yy, batch_size=None, verbose=0, steps=None)
     print(prediction)
-    return prediction
+    return prediction"""
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     
     return 'atef dahmen'
-@app.route('/j',methods=['POST','GET'])
+"""@app.route('/j',methods=['POST','GET'])
 def hello_world():
     req_data = request.get_json()
     a = req_data['a']
@@ -26,7 +26,7 @@ def hello_world():
     d = req_data['d']
     f = req_data['f']
     x = predict(float(a),float(b),float(c),float(d),float(f))[0][0]
-    return jsonify(str(x))
+    return jsonify(str(x))"""
 
 
 if __name__ == '__main__':
